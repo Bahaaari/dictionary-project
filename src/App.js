9ebb5040565e32d14419483b9c45cc7ed2fb4fd0
Bahaaari <87390415+Bahaaari@users.noreply.github.com>
2021-12-05 22:25:17 +0330

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import logo from "./logo.png";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+      </div>
+    </div>
+  );
+}
+
+export default App;
