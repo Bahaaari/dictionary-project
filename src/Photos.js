ec51452cfa7f13eecd7c7f8dba089c2751f94c7b
Bahaaari <87390415+Bahaaari@users.noreply.github.com>
2021-12-20 10:13:12 +0330

    complete Photos component

--- src/Photos.js
+++ src/Photos.js
@@ -1,15 +1,19 @@
 import React from "react";
+import "./Photos.css";
 
 export default function Photos(props) {
   if (props.photos) {
-    console.log(props.photos);
     return (
       <section className="photos">
         <div className="row">
           {props.photos.map(function (photo, index) {
             return (
-              <div className="col-3" key={index}>
-                <img src={photo.src.landscap} className="img-fluid" />
+              <div className="col-4" key={index}>
+                <img
+                  src={photo.src.landscape}
+                  alt="pexels-searching"
+                  className="img-fluid"
+                />
               </div>
             );
           })}
